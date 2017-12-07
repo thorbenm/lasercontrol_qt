3c7ef05cfa006beadc3a7baa533b09170689c71e
Thorben B. Muehleder <thorbenm@me.com>
2017-12-07 09:47:52 +0100

    spi -> i2c

--- lasercontrol_qt/mainwindow.cpp
+++ lasercontrol_qt/mainwindow.cpp
@@ -17,8 +17,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_horizontalSlider_sliderMoved(int position)
 {
-    ui->lcdNumber->display((double)position / 100.0);
+   // ui->lcdNumber->display((double)position / 100.0);
   //  transmit_voltage(current_to_voltage(position / 100.0));
+    //  result = wiringPiI2CWriteReg16(current, 0x0000, position) ;
+//XX    wiringPiI2CWriteReg16(current, 0x0000, position) ;
 }
 
 void MainWindow::on_lcdNumber_overflow()
